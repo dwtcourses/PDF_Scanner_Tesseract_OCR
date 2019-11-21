e0cbbdee7ce9989cf6a79b5706728aadde0e6885
Eric035 <ericleung1997@gmail.com>
2019-11-21 17:04:27 -0500

    Updated migration file

--- PDF2News/backend/migrations/20191121214408-create-pdf.js
+++ PDF2News/backend/migrations/20191121214408-create-pdf.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Pdf', {
+    return queryInterface.createTable('Pdfs', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Pdf');
+    return queryInterface.dropTable('Pdfs');
   }
 };
